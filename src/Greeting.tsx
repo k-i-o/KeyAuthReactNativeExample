d42c097464fb5db74bd6029c724b87ae7c3f071c
k-i-o <sradici26@gmail.com>
2024-01-25 02:49:58 +0100

    first commit

---
+++ src/Greeting.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Text, View, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  center: {
+    alignItems: 'center',
+  },
+});
+
+type GreetingProps = {
+  name: string;
+};
+
+const Greeting = (props: GreetingProps) => {
+  return (
+    <View style={styles.center}>
+      <Text>Hello {props.name}!</Text>
+    </View>
+  );
+};
+
+const LotsOfGreetings = () => {
+  return (
+    <View style={[styles.center, {top: 50}]}>
+      <Greeting name="Rexxar" />
+      <Greeting name="Jaina" />
+      <Greeting name="Valeera" />
+    </View>
+  );
+};
+
+export default LotsOfGreetings;
