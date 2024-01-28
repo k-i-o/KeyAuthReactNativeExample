c53634cd99de9e0be13be5baa9bd0056d2d89ce5
k-i-o <sradici26@gmail.com>
2024-01-29 00:55:43 +0100

    click feedback

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,11 +1,29 @@
-import { ActivityIndicator, Pressable, Text } from "react-native";
+import { ActivityIndicator, Animated, Pressable, Text } from "react-native";
 import { styles } from "../../styles";
 
+const animated = new Animated.Value(1);
 export default CustomButton = ({ title, onPress, style, loading = false }) => {
+
+  const fadeIn = () => {
+    Animated.timing(animated, {
+      toValue: 0.1,
+      duration: 100,
+      useNativeDriver: true,
+    }).start();
+  };
+  const fadeOut = () => {
+    Animated.timing(animated, {
+      toValue: 1,
+      duration: 200,
+      useNativeDriver: true,
+    }).start();
+  };
+
   return (
-    <Pressable style={{...styles.button, ...style}} onPress={onPress} >
+    <Pressable style={{...styles.button, ...style}} onPressIn={fadeIn} onPressOut={fadeOut} onPress={onPress} >
       {loading ? 
-        <ActivityIndicator size="large" color="#fff"/> : <Text style={styles.text}>{title}</Text>
+        <ActivityIndicator size="large" color="#fff" /> : 
+        <Animated.Text style={{...styles.text, opacity: animated}}>{title}</Animated.Text>
       }
     </Pressable>
   );
